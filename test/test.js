5fdff6813de8ce9e27f1e684863fc87b6fc50993
Ricky Reusser <rsreusser@gmail.com>
2015-04-30 14:08:35 -0400

    Add dtype option

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var zeros = require("../zeros.js")
 
-require("tape")("ndarray zeros", function(t) {
+var test = require("tape");
+
+test("ndarray zeros", function(t) {
 
   var x = zeros([64,32])
   
@@ -8,5 +10,71 @@ require("tape")("ndarray zeros", function(t) {
   t.equals(x.size, 64*32)
   t.equals(x.dtype, "float64")
 
+  t.end()
+});
+
+test("ndarray uint8 zeros", function(t) {
+  var x = zeros([64,32], 'uint8')
+  t.equals( x.dtype, "uint8")
+  t.end()
+})
+
+test("ndarray uint16 zeros", function(t) {
+  var x = zeros([64,32], 'uint16')
+  t.equals( x.dtype, "uint16")
+  t.end()
+})
+
+test("ndarray uint32 zeros", function(t) {
+  var x = zeros([64,32], 'uint32')
+  t.equals( x.dtype, "uint32")
+  t.end()
+})
+
+test("ndarray int8 zeros", function(t) {
+  var x = zeros([64,32], 'int8')
+  t.equals( x.dtype, "int8")
+  t.end()
+})
+
+test("ndarray int16 zeros", function(t) {
+  var x = zeros([64,32], 'int16')
+  t.equals( x.dtype, "int16")
+  t.end()
+})
+
+test("ndarray int32 zeros", function(t) {
+  var x = zeros([64,32], 'int32')
+  t.equals( x.dtype, "int32")
+  t.end()
+})
+
+test("ndarray double zeros", function(t) {
+  var x = zeros([64,32], 'double')
+  t.equals( x.dtype, "float64")
+  t.end()
+})
+
+test("ndarray float zeros", function(t) {
+  var x = zeros([64,32], 'float')
+  t.equals( x.dtype, "float32")
+  t.end()
+})
+
+test("ndarray uint8_clamped zeros", function(t) {
+  var x = zeros([64,32], 'uint8_clamped')
+  t.equals( x.dtype, "uint8_clamped")
+  t.end()
+})
+
+test("ndarray array zeros", function(t) {
+  var x = zeros([64,32], 'array')
+  t.equals( x.dtype, "array")
+  t.end()
+})
+
+test("ndarray array buffer zeros", function(t) {
+  var x = zeros([64,32], 'buffer')
+  t.equals( x.dtype, "generic")
   t.end()
 })
--- zeros.js
+++ zeros.js
@@ -2,10 +2,43 @@
 
 var ndarray = require("ndarray")
 
-module.exports = function zeros(shape) {
-  var sz = 1
-  for(var i=0; i<shape.length; ++i) {
-    sz *= shape[i]
+function dtypeToType(dtype) {
+  switch(dtype) {
+    case 'uint8':
+      return Uint8Array;
+    case 'uint16':
+      return Uint16Array;
+    case 'uint32':
+      return Uint32Array;
+    case 'int8':
+      return Int8Array;
+    case 'int16':
+      return Int16Array;
+    case 'int32':
+      return Int32Array;
+    case 'float':
+    case 'float32':
+      return Float32Array;
+    case 'double':
+    case 'float64':
+      return Float64Array;
+    case 'uint8_clamped':
+      return Uint8ClampedArray;
+    case 'generic':
+    case 'buffer':
+    case 'data':
+    case 'dataview':
+      return ArrayBuffer;
+    case 'array':
+      return Array;
   }
-  return ndarray(new Float64Array(sz), shape)
+}
+
+module.exports = function zeros(shape, dtype) {
+  dtype = dtype || 'float64';
+  var sz = 1;
+  for(var i=0; i<shape.length; ++i) {
+    sz *= shape[i];
+  }
+  return ndarray(new (dtypeToType(dtype))(sz), shape);
 }
